48f21360b66fad4f152153e8559a41ddc2399c6f
Rizky <rizkyaditya2106@gmail.com>
2023-05-15 13:56:03 +0700

    adjusting

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -446,72 +446,72 @@ function submitForm(e, evt) {
 }
 
 // Latest Output Chart
-var latestOutput='';
+// var latestOutput='';
 
-var options = {
-    series: [
-        {
-            name: "High - 2013",
-            data: [28, 29, 33, 36, 32, 32, 33]
-        },
-        {
-            name: "Low - 2013",
-            data: [12, 11, 14, 18, 17, 13, 13]
-        }
-    ],
-    chart: {
-        height: 350,
-        type: 'line',
-        dropShadow: {
-            enabled: true,
-            color: '#000',
-            top: 18,
-            left: 7,
-            blur: 10,
-            opacity: 0.2
-        },
-        toolbar: {
-            show: false
-        }
-    },
-    colors: ['#77B6EA', '#545454'],
-    dataLabels: {
-        enabled: true,
-    },
-    stroke: {
-        curve: 'smooth'
-    },
-    grid: {
-        borderColor: '#e7e7e7',
-        row: {
-            colors: ['#f3f3f3', 'transparent'],
-            opacity: 0.5
-        },
-    },
-    markers: {
-        size: 1
-    },
-    xaxis: {
-        categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul'],
-        title: {
-            text: 'Month'
-        }
-    },
-    yaxis: {
-        min: 5,
-        max: 40
-    },
-    legend: {
-        position: 'top',
-        horizontalAlign: 'right',
-        floating: true,
-        offsetY: -25,
-        offsetX: -5
-    }
-};
+// var options = {
+//     series: [
+//         {
+//             name: "High - 2013",
+//             data: [28, 29, 33, 36, 32, 32, 33]
+//         },
+//         {
+//             name: "Low - 2013",
+//             data: [12, 11, 14, 18, 17, 13, 13]
+//         }
+//     ],
+//     chart: {
+//         height: 350,
+//         type: 'line',
+//         dropShadow: {
+//             enabled: true,
+//             color: '#000',
+//             top: 18,
+//             left: 7,
+//             blur: 10,
+//             opacity: 0.2
+//         },
+//         toolbar: {
+//             show: false
+//         }
+//     },
+//     colors: ['#77B6EA', '#545454'],
+//     dataLabels: {
+//         enabled: true,
+//     },
+//     stroke: {
+//         curve: 'smooth'
+//     },
+//     grid: {
+//         borderColor: '#e7e7e7',
+//         row: {
+//             colors: ['#f3f3f3', 'transparent'],
+//             opacity: 0.5
+//         },
+//     },
+//     markers: {
+//         size: 1
+//     },
+//     xaxis: {
+//         categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul'],
+//         title: {
+//             text: 'Month'
+//         }
+//     },
+//     yaxis: {
+//         min: 5,
+//         max: 40
+//     },
+//     legend: {
+//         position: 'top',
+//         horizontalAlign: 'right',
+//         floating: true,
+//         offsetY: -25,
+//         offsetX: -5
+//     }
+// };
 
-var chart = new ApexCharts(document.querySelector("#daily-chart"), options);
-chart.render();
+// var chart = new ApexCharts(document.querySelector("#daily-chart"), options);
+// chart.render();
 
 // Select Defect Area
 function showSelectDefectArea(defectAreaImage) {
