8a5a9495697b0b3094eb22eee2faed8a2522f7cf
Rizky <rizkyaditya2106@gmail.com>
2023-05-24 17:32:36 +0700

    adjusting

--- app/Http/Requests/LoginRequest.php
+++ app/Http/Requests/LoginRequest.php
@@ -29,4 +29,17 @@ class LoginRequest extends FormRequest
             'remember' => 'nullable'
         ];
     }
+
+    /**
+     * Get the error messages for the defined validation rules.
+     *
+     * @return array<string, string>
+     */
+    public function messages(): array
+    {
+        return [
+            'username.required' => 'Harap masukkan <b>username</b>',
+            'password.required' => 'Harap masukkan <b>password</b> ',
+        ];
+    }
 }
