f93e9a8908c9bc46c6d62e5a6a56f65c164c0af3
Rizky <rizkyaditya2106@gmail.com>
2023-08-18 11:03:33 +0700

    fix rework's defect id being null bug

--- app/Http/Livewire/Rework.php
+++ app/Http/Livewire/Rework.php
@@ -123,7 +123,7 @@ class Rework extends Component
     }
 
     public function submitAllRework() {
-        $allDefect = Defect::selectRaw('output_defects.id defect_id, output_defects.master_plan_id, output_defects.so_det_id so_det_id')->
+        $allDefect = Defect::selectRaw('output_defects.id id, output_defects.master_plan_id master_plan_id, output_defects.so_det_id so_det_id')->
             leftJoin('so_det', 'so_det.id', '=', 'output_defects.so_det_id')->
             where('output_defects.defect_status', 'defect')->
             where('output_defects.master_plan_id', $this->orderInfo->id)->get();
