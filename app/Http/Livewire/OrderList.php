08aab3ab817e9b356f896398a41656550bc8c98a
it-nag <ramon@ptnag.com>
2024-09-04 07:46:48 +0700

    adjust master plan

--- app/Http/Livewire/OrderList.php
+++ app/Http/Livewire/OrderList.php
@@ -33,7 +33,7 @@ class OrderList extends Component
 
     public function render()
     {
-        $masterPlanBefore = MasterPlan::select("id")->where("sewing_line", strtoupper(Auth::user()->username))->where("master_plan.cancel", "N")->where("tgl_plan", "<", $this->date)->orderBy("tgl_plan", "desc")->first();
+        $masterPlanBefore = MasterPlan::selectRaw("max(id) as id")->where("sewing_line", strtoupper(Auth::user()->username))->where("master_plan.cancel", "N")->where("tgl_plan", "<", $this->date)->groupBy("master_plan.id_ws", "master_plan.color")->orderBy("tgl_plan", "desc")->first();
 
         $additionalQuery = "";
         if ($masterPlanBefore) {
