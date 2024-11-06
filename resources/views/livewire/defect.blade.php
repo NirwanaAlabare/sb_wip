9ce6866060c71b527ebf1f30d67709749e0244f9
Rizky <rizkyaditya2106@gmail.com>
2024-11-06 10:44:35 +0700

    disable undo module

--- resources/views/livewire/defect.blade.php
+++ resources/views/livewire/defect.blade.php
@@ -57,7 +57,7 @@
                             <p class="mb-1 fs-5">:</p>
                             <p id="defect-qty" class="mb-1 fs-5">{{ $output->count() }}</p>
                         </div>
-                        <button class="btn btn-dark" wire:click="$emit('preSubmitUndo', 'defect')">
+                        <button class="btn btn-dark" wire:click="$emit('preSubmitUndo', 'defect')" disabled>
                             <i class="fa-regular fa-rotate-left"></i>
                         </button>
                         {{-- <button class="btn btn-dark">
